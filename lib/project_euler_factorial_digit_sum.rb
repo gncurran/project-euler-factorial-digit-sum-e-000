87c47f754dc80789977faab0ff7998c1bc74f75c
gncurran <gnc115@gmail.com>
2015-12-06 14:56:31 -0500

    Done.

--- lib/project_euler_factorial_digit_sum.rb
+++ lib/project_euler_factorial_digit_sum.rb
@@ -1,1 +1,12 @@
-# code your solution here
+# Write a method that returns the factorial of the number passed.
+def factorial(n)
+  (1..n).reduce(:*)
+end
+
+def sum_of_digits(n)
+  n.to_s.chars.map(&:to_i).reduce(:+)
+end
+
+def factorial_digit_sum(n)
+  sum_of_digits(factorial(n))
+end
